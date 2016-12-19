3e43fdd454f0aae0b2b020ed2cbda9202efad292
MarciaOrr <orrmar@swcp.com>
2016-12-19 23:45:53 +0000

    User Permission: helper_method and before_action method. refactoring

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,16 +1,22 @@
 class Instructor::SectionsController < ApplicationController
   def new
-    @course = Course.find(params[:course_id])
+    @course = current_course
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
+    @course = current_course
     @section = @course.sections.create(section_params)
     redirect_to instructor_course_path(@course)
   end
 
   private
+
+  helper_method :current_course
+  def current_course
+    @course ||= Course.find(params[:course_id])
+  end
+
   def section_params
     params.require(:section).permit(:title)
   end
