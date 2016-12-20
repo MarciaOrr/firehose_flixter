9f60efda3cb07bef908c076923e906170b38a398
MarciaOrr <orrmar@swcp.com>
2016-12-20 00:29:30 +0000

    helper and before_action for instructor course page

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,20 +1,26 @@
 class Instructor::SectionsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_course
+
   def new
-    @course = current_course
     @section = Section.new
   end
 
   def create
-    @course = current_course
-    @section = @course.sections.create(section_params)
+    @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
 
   private
-
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render text: 'Unauthorzed', status: :unauthorized
+    end
+  end
+  
   helper_method :current_course
   def current_course
-    @course ||= Course.find(params[:course_id])
+    @current_course ||= Course.find(params[:course_id])
   end
 
   def section_params
